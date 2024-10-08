d315ad6ad660487118bfaffd29bed1fd0a03fdf2
byhexdev <onionfinder15@gmail.com>
2024-10-08 16:15:32 +0700

    r

--- marzban/api.py
+++ marzban/api.py
@@ -126,9 +126,9 @@ class MarzbanAPI:
         response = await self._request("POST", url, token)
         return UserResponse(**response.json())
 
-    async def get_users(self, token: str, offset: Optional[int] = None, limit: Optional[int] = None, username: Optional[List[str]] = None, status: Optional[str] = None, sort: Optional[str] = None) -> UsersResponse:
+    async def get_users(self, token: str, offset: Optional[int] = None, limit: Optional[int] = None, username: Optional[List[str]] = None, status: Optional[str] = None, sort: Optional[str] = None, search: Optional[str] = None) -> UsersResponse:
         url = "/api/users"
-        params = {"offset": offset, "limit": limit, "username": username, "status": status, "sort": sort}
+        params = {"offset": offset, "limit": limit, "username": username, "status": status, "sort": sort, "search": search}
         response = await self._request("GET", url, token, params=params)
         return UsersResponse(**response.json())
 
